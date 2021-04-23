86ecf7d5c771c3e4617b324e0c2ab6300b7fb0ec
cegprakash <cegprakash@gmail.com>
2021-04-23 15:49:27 +0530

    rest standard changes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,15 @@
 class CommentsController < ApplicationController
     def post_comment
-        params.require(:from_user_id)
-        params.require(:to_user_id)
-        params.require(:comment)
-        from_user_id = params[:from_user_id]
+        params.require(:user)        
         to_user_id =  params[:to_user_id]
-        comment = params[:comment]
+        user = params[:user]
+       
         commentRecord = Comment.create({
-            :from_user_id => from_user_id,
+            :from_user_id =>user['comment']['from_user_id'],
             :to_user_id => to_user_id,
-            :comment => comment,
+            :comment => user['comment']['comment'],
         })
-        all_comments = Comment.from_user(from_user_id).to_user(to_user_id).order("created_at DESC").page(1).per(5)
+        all_comments = Comment.from_user(user['comment']['from_user_id']).to_user(to_user_id).order("created_at DESC").page(1).per(5)
         render json: { comment: all_comments }, status: :ok
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   delete '/api/v1/users/:user_id' => 'users#delete'
   put '/api/v1/users/:user_id' => 'users#update'
   post '/api/v1/users' => 'users#create'
-  post '/api/v1/comments' => 'comments#post_comment'
+  post '/api/v1/users/:to_user_id/comments' => 'comments#post_comment'
 end
