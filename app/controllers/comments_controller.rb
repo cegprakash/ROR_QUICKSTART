51fc129f24773fdca34e53218e52c4be336fc364
cegprakash <cegprakash@gmail.com>
2021-04-28 06:32:29 +0530

    nested attribute save

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,11 +4,20 @@ class CommentsController < ApplicationController
         to_user_id =  params[:to_user_id]
         user = params[:user]
        
-        commentRecord = Comment.create({
-            :from_user_id =>user['comment']['from_user_id'],
-            :to_user_id => to_user_id,
-            :comment => user['comment']['comment'],
-        })
+        # commentRecord = Comment.create({
+        #     :from_user_id =>user['comment']['from_user_id'],
+        #     :to_user_id => to_user_id,
+        #     :comment => user['comment']['comment'],
+        # })
+
+        logged_in_user = User.find_by_id(user['comment']['from_user_id'])
+
+        if logged_in_user
+            user_comment = logged_in_user.comments_posted.create()
+            user_comment.to_user = User.find_by_id(to_user_id)
+            user_comment.comment = user['comment']['comment']
+            user_comment.save
+        end
 
         render json: {}, status: :ok
     end
